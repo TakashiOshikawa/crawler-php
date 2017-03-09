7acce3cedc7ad0714a241f5e6d2e98cb2414c27a
root <root@d881f4230ce0.(none)>
2017-03-09 09:49:40 +0000

    Delete GuzzleWrapper

--- src/executer/Main.php
+++ src/executer/Main.php
@@ -4,7 +4,6 @@ require 'vendor/autoload.php';
 
 use GuzzleHttp\Client;
 use Mycrawler\Crawler;
-use Mycrawler\GuzzleWrapper;
 use Mycrawler\GoutteWrapper;
 
 
@@ -15,15 +14,3 @@ $crawler
     ->run();
 
 var_dump($crawler->getUrls());
-/*$client = new Client([
-    // Base URI is used with relative requests
-    'base_uri' => 'http://httpbin.org',
-    // You can set any number of default request options.
-    'timeout'  => 2.0,
-]);
-
-
-$crawler = new Crawler();
-$crawler->echoArgString('test');
-echo "\n";
- */
